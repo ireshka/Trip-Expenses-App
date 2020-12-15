99197d1c0827c12bc45e99779416d65ce0faf75d
ireshka <ireshka@users.noreply.github.com>
2020-12-15 22:05:45 +0100

    Fix - restore icons

--- client/src/template/MainTemplate/MainTemplate.component.jsx
+++ client/src/template/MainTemplate/MainTemplate.component.jsx
@@ -1,8 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowLeft, faDollarSign, faWallet } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 import Footer from '../../components/Footer/Footer.component';
 import Header from '../../components/Header/Header.component';
 import { StyledWrapper } from './MainTemplate.styles';
+
+library.add(faWallet, faDollarSign, faArrowLeft);
 
 export const MainTemplate = ({ children }) => {
   return (
